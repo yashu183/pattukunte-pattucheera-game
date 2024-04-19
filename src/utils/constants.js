7f0557b568f20e2c892d6080d87aac0cb4a93e08
Nipuna89 <156865687+Nipuna89@users.noreply.github.com>
2024-04-18 20:48:42 -0700

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -278,5 +278,10 @@ export const missingMovies = [
   "Hungama",
   "Prema Entha Madhuram",
   "Eagle",
-  "Maestro"
+  "Maestro",
+  "Gaami",
+  "Geethanjali Malli Vachindi",
+  "The Family Star",
+  "Premalu",
+  "Om Bheem Bush"
 ];
